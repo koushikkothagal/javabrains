90f9790ae36edabd33a3a5b7b5107a5c95ddc72c
Koushik Kothagal <koush.s@gmail.com>
2015-09-28 23:14:57 -0700

    Homepage improvements

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,12 +6,13 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($rootScope) {
+  function MainController($rootScope, courseDataService) {
     var vm = this;
     $rootScope.page = {
       'title': 'Java Brains',
       'desc': 'Learn and advance your Java and JavaScript skills online'
     };
-    
+    vm.topics = courseDataService.topics;
+    vm.courses = courseDataService.courses;
   }
 })();
